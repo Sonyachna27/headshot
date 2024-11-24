48a567e61b81b07f38209e88bdd19f12a33683a0
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-11-24 18:02:19 +0200

    add pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () =>{
 	playVideo();
 	initPlotSlider();
 	adjustPadding();
+	initReviewsSlider();
+	smooth();
 });
 const HTMLELEMENT = document.querySelector('html');
 const openHeader = () =>{
@@ -22,6 +24,27 @@ navLinks.forEach((link) =>{
 		HTMLELEMENT.classList.remove('open');
 	})
 })
+}
+const smooth = () =>{
+	document.querySelectorAll('a[href^="#"').forEach(link => {
+
+		link.addEventListener('click', function(e) {
+				e.preventDefault();
+	
+				let href = this.getAttribute('href').substring(1);
+	
+				const scrollTarget = document.getElementById(href);
+	
+				const topOffset = document.querySelector('header').offsetHeight;
+				const elementPosition = scrollTarget.getBoundingClientRect().top;
+				const offsetPosition = elementPosition - topOffset;
+	
+				window.scrollBy({
+						top: offsetPosition,
+						behavior: 'smooth'
+				});
+		});
+	});
 }
 const openSideBar = () => {
 	const sidebarBtn = document.querySelector('.sidebar__in');
@@ -70,30 +93,29 @@ const accordionFunction = () => {
 };
 AOS.init();
 
-const playVideo = () =>{
+const playVideo = () => {
 	const startVideoBtns = document.querySelectorAll('.start-video');
-	const videos = document.querySelectorAll('video');
 
 	startVideoBtns.forEach((startBtn) => {
 			startBtn.addEventListener('click', () => {
-				
-					const btnData = startBtn.getAttribute('data-btn');
-					videos.forEach((video) => {
-							const videoData = video.getAttribute('data-video');
-							const videoPosterAtribute = video.getAttribute('poster');
+					const wrapper = startBtn.closest('.different__frame'); 
+					const video = wrapper.querySelector('video'); 
 
-							if (btnData === videoData) {
-									if (video.paused) {
-											video.play();
-											video.removeAttribute('poster');
-											startBtn.classList.add('play');
-									} else {
-											video.pause();
-											video.setAttribute('poster', `${videoPosterAtribute}`);
-											startBtn.classList.remove('play');
-									}
+					if (video) {
+						const videoPosterAtribute = video.getAttribute('poster');
+							if (video.paused) {
+									video.play();
+									video.style.height = "100%";
+									video.style.width = "100%";
+									startBtn.classList.add('play');
+									video.removeAttribute('poster');
+							} else {
+									video.pause();
+									startBtn.classList.remove('play');
+									video.setAttribute('poster', `${videoPosterAtribute}`);
+									startBtn.classList.remove('play');
 							}
-					});
+					}
 			});
 	});
 };
@@ -105,6 +127,7 @@ const initPlotSlider = () =>{
 
 		pagination: {
 			el: ".plot-pagination",
+			clickable:true,
 		},
 		navigation: {
 			nextEl: ".plot-button-next",
@@ -120,6 +143,26 @@ const initPlotSlider = () =>{
 			1023: {
 				slidesPerView: 3,
 			}
+		}
+	});
+}
+const initReviewsSlider = () =>{
+	const reviewsSliderWrap = document.querySelector('.reviewsSlider');
+	if(!reviewsSliderWrap) return
+	const reviewsSlider = new Swiper(reviewsSliderWrap, {
+
+		pagination: {
+			el: ".reviews-pagination",
+			clickable:true,
+		},
+	
+		slidesPerView: 1,
+		spaceBetween: 20,
+		breakpoints: {
+			1023: {
+				slidesPerView: 2,
+			},
+			
 		}
 	});
 }
@@ -141,3 +184,68 @@ const adjustPadding = () => {
 
 window.addEventListener('resize', adjustPadding);
 
+const reviewsFormChecked = () =>{
+	if (document.querySelector(".reviewForm")) {
+    const commentForm = document.getElementById("comment-form");
+    const commentName = document.getElementById("name");
+    const commentSurname = document.getElementById("lastName");
+    const commentText = document.getElementById("message");
+    const sick = document.getElementById("sick");
+    const commentButton = document.getElementById("comment-button");
+
+    commentButton.addEventListener("click", function (e) {
+      const ratingElement = document.querySelector(
+        'input[name="rating"]:checked'
+      );
+      const rating = ratingElement ? ratingElement.value : "";
+      const commentPost = this.getAttribute("data-post-id");
+      e.preventDefault();
+      var xhr = new XMLHttpRequest();
+      if (commentName.value && commentSurname && commentText) {
+        xhr.open("POST", homepage_js.ajax_url, true);
+        xhr.setRequestHeader(
+          "Content-Type",
+          "application/x-www-form-urlencoded"
+        );
+        xhr.send(
+          "comment_name=" +
+            commentName.value +
+            "&comment_surname=" +
+            commentSurname.value +
+            "&rating=" +
+            rating +
+            "&sick=" +
+            sick.value +
+            "&comment_text=" +
+            commentText.value +
+            "&commentPost=" +
+            commentPost +
+            "&action=new_comment_function"
+        );
+        xhr.onload = function () {
+          if (this.status >= 200 && this.status < 400) {
+            commentForm.reset();
+            alert(
+              homepage_js.currentLang === "uk"
+                ? "Ваш відгук було надіслано"
+                : "Ваш отзыв был отправлен"
+            );
+            // commentForm.textContent = '';
+          } else {
+            alert(
+              homepage_js.currentLang === "uk"
+                ? "Заповніть форму"
+                : "Заполните форму"
+            );
+          }
+        };
+      } else {
+        alert(
+          homepage_js.currentLang === "uk"
+            ? "Заповніть форму"
+            : "Заполните форму"
+        );
+      }
+    });
+  }
+}
